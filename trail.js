f1c929a2b4b16121aa484e489ef020327960fbe1
ishestak <shestak.irina@gmail.com>
2014-09-30 07:54:43 -0700

    markers set up

--- trail.js
+++ trail.js
@@ -1,25 +1,48 @@
+var map;
 var marker;
 function initialize() {
   var route1Latlng = new google.maps.LatLng(49.279933, -123.109557);
   var mapOptions = {
      center: route1Latlng,
-     zoom: 8,
+     zoom: 12,
      mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
   $.ajax({
    type: "GET",
-   url: "crabpark.gpx",
+   url: "Seawall_Race.gpx",
    dataType: "xml",
    success: function (xml){ 
+     var start;
+     var end;
      var points = [];
      var bounds = new google.maps.LatLngBounds();
-     $(xml).find("trkpt").each(function () {
+     $(xml).find("trkpt").each(function() {
        var lat = $(this).attr("lat");
        var lon = $(this).attr("lon");
-       var start = $(this).first();
-       var end = $(this).last();
+       var startRoute = function(){
+        latStart = $(this).first().attr("lat");
+        lonStart = $(this).first().attr("lon");
+        var s = new google.maps.LatLng(latStart, lonStart);
+         var marker = new google.maps.Marker({
+          map:map,
+          position: s,
+          title: "I start my run here, woop!"          
+         });
+       console.log(s);
+       } 
+       var endStart = function(){
+        latEnd = $(this).last().attr("lat");
+        latEnd = $(this).first().attr("lon");
+        var e = new google.maps.LatLng(latEnd, lonEnd);
+         var marker = new google.maps.Marker({
+          map:map,
+          position: e,
+          title: "I end my run here, woop!"          
+         });
+        console.log(e);
+       } 
        var p = new google.maps.LatLng(lat, lon);
        points.push(p);
        bounds.extend(p);
@@ -30,8 +53,6 @@ function initialize() {
        strokeOpacity: .5,
        strokeWeight: 2
      });
-     var marker = new google.maps.maps.Marker
-
      poly.setMap(map);
      map.fitBounds(bounds);
    }
